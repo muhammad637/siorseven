69896f502bf8c9220940b2f4ec427b9d815b6e70
Ilhamrsdi <muhamadilhamrosidi0707@gmail.com>
2023-05-16 13:07:36 +0700

    fixed crud barang

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,15 +16,16 @@
                         <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
                             <div class="card card-plain">
                                 <div class="card-header pb-0 text-start">
-                                    <h4 class="font-weight-bolder">Sign In</h4>
-                                    <p class="mb-0">Enter your email and password to sign in</p>
+                                    <h4 class="font-weight-bolder">Login</h4>
+                                    <p class="mb-0">Masukkan username dan password 
+                                        untuk masuk </p>
                                 </div>
                                 <div class="card-body">
                                     <form role="form" method="POST" action="{{ route('login.perform') }}">
                                         @csrf
                                         @method('post')
                                         <div class="flex flex-col mb-3">
-                                            <input type="text" name="username" class="form-control form-control-lg" value="{{ old('username') ?? 'admin' }}" aria-label="Email">
+                                            <input type="text" name="username" class="form-control form-control-lg" value="{{ old('username') ?? 'admin' }}" aria-label="Username">
                                             @error('username') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
                                         </div>
                                         <div class="flex flex-col mb-3">
@@ -36,28 +37,23 @@
                                             <label class="form-check-label" for="rememberMe">Remember me</label>
                                         </div>
                                         <div class="text-center">
-                                            <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Sign in</button>
+                                            <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Login</button>
                                         </div>
                                     </form>
                                 </div>
                                 <div class="card-footer text-center pt-0 px-lg-2 px-1">
                                     <p class="mb-1 text-sm mx-auto">
-                                        Forgot you password? Reset your password
-                                        <a href="{{ route('reset-password') }}" class="text-primary text-gradient font-weight-bold">here</a>
+                                        Lupa Password Anda ? Silahkan Reset Password 
+                                        <a href="{{ route('reset-password') }}" class="text-primary text-gradient font-weight-bold">Disini</a>
                                     </p>
                                 </div>
-                                <div class="card-footer text-center pt-0 px-lg-2 px-1">
-                                    <p class="mb-4 text-sm mx-auto">
-                                        Don't have an account?
-                                        <a href="{{ route('register') }}" class="text-primary text-gradient font-weight-bold">Sign up</a>
-                                    </p>
-                                </div>
+                                
                             </div>
                         </div>
                         <div
                             class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-                            <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
-                                style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg');
+                            <div class="position-relative bg-gradient-primary h-100 m-2 px-10 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
+                                style="background-image: url('https://svgshare.com/i/t8g.svg');
               background-size: cover;">
                                 <span class="mask bg-gradient-primary opacity-6"></span>
                                 <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new
